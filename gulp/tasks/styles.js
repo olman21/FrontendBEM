26333e42943611d0159d0506a0c1e48fc16333b6
Olman <olmanmora21@gmail.com>
2017-01-08 16:46:07 -0600

    Setup postcss-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ var gulp = require("gulp"),
     autoPrex = require('autoprefixer'),
     cssVars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    importCss = require('postcss-import');
+    importCss = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 gulp.task("styles",function(){
     return gulp.src('./app/styles/styles.css')
-        .pipe(postcss([importCss,cssVars,nested,autoPrex]))
+        .pipe(postcss([importCss,mixins,cssVars,nested,autoPrex]))
         .on("error",function(error){
             console.log(error);
             this.emit('end');
